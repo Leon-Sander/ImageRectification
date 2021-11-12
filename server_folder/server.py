2fcdcd837a005c9e9f011c4305d83c73ba8f6897
Leon Sander <leon.sander@protonmail.com>
2021-11-13 00:21:53 +0100

    image sending and displaying on same page now

--- server_folder/server.py
+++ server_folder/server.py
@@ -6,13 +6,15 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir) 
 
 import io
-from flask import Flask, request, render_template, Response, send_file
+from flask import Flask, request, render_template, send_file
 from models.full_model import crease
 from server_folder import preprocess
 import torch
 from PIL import Image
 import cv2
 import logging
+import base64
+
 app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 model = crease()
@@ -32,15 +34,19 @@ def rectify():
     preprocessed_img = preprocess.preprocess_img(image_file)
     bm_prediction = model.forward(preprocessed_img)
     unwarped_img = model.unwarp_image(preprocessed_img,bm_prediction.transpose(1,2).transpose(2,3))
-    #app.logger.info(unwarped_img)
     unwarped_img = unwarped_img * 255
+    #cv2.resize(unwarped_img, )
     img = Image.fromarray(unwarped_img.astype('uint8'))
+    
     file_object = io.BytesIO()
     img.save(file_object, 'PNG')
     file_object.seek(0)  # rewind your buffer
+    #app.logger.info(dir(img))
 
-    return send_file(file_object, mimetype='image/PNG')
-    #return render_template('index.html', plot_url=send_file(file_object, mimetype='image/PNG'))
+    data = base64.b64encode(file_object.getbuffer()).decode('ascii')
+
+    return render_template('index.html', plot_url=data)
+
 
 
 if __name__ == "__main__":
